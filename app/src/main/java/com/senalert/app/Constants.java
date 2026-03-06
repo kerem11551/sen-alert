0ac56142e8512957022044fb6c7f75cc73cb3006
kerem11551 <kerem115511@gmail.com>
2026-03-06 11:34:05 +0300

    Create Constants.java

---
+++ app/src/main/java/com/senalert/app/Constants.java
@@ -0,0 +1,12 @@
+package com.senalert.app;
+
+public class Constants {
+    public static final String CHANNEL_ID   = "SEN_ALERT_CHANNEL";
+    public static final String ACTION_STATE = "com.senalert.app.STATE";
+    public static final String EXTRA_STATE  = "state";
+    public static final String EXTRA_X      = "x";
+    public static final String EXTRA_Y      = "y";
+    public static final String EXTRA_Z      = "z";
+    public static final String EXTRA_DXY    = "dxy";
+    public static final String EXTRA_DZ     = "dz";
+}
